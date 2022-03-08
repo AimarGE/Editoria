3206289123c5d2f5e02ab0d74f243abb743f92ee
AimarGE <aimar5454@gmail.com>
2022-03-08 15:26:55 +0100

    Primer commit Aimar

--- app/src/main/java/com/example/editoria/RegisterCliente.java
+++ app/src/main/java/com/example/editoria/RegisterCliente.java
@@ -3,10 +3,18 @@ package com.example.editoria;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class RegisterCliente extends AppCompatActivity {
+
+    EditText usuario;
+    EditText password;
+    EditText confirmPassword;
+    EditText email;
+    EditText telefono;
+    EditText fechaNacimiento;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +26,9 @@ public class RegisterCliente extends AppCompatActivity {
 
 
     public void register(View view) {
+
+
+
         Intent intent = new Intent(this, Login.class);
         startActivity(intent);
     }
