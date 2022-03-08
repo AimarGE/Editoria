664a4c8f8541324d431e1be5673eb6295afdf512
AimarGE <aimar5454@gmail.com>
2022-03-08 16:14:07 +0100

    Iniciando comprobaciones de los datos de registro cliente.

--- app/src/main/java/com/example/editoria/RegisterCliente.java
+++ app/src/main/java/com/example/editoria/RegisterCliente.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -25,11 +26,23 @@ public class RegisterCliente extends AppCompatActivity {
     }
 
 
-    public void register(View view) {
-
-
-
+    public void register(View _) {
+        usuario = (EditText) findViewById(R.id.usuario);
+        password = (EditText) findViewById(R.id.password);
+        confirmPassword= (EditText) findViewById(R.id.ConfirmPassword);
+        email = (EditText) findViewById(R.id.email);
+        telefono = (EditText) findViewById(R.id.telefono);
+        fechaNacimiento = (EditText) findViewById(R.id.FechaNacimiento);
+        comprobacionDatos();
         Intent intent = new Intent(this, Login.class);
         startActivity(intent);
+    }
+
+    private void comprobacionDatos(){
+        comprobarContras();
+    }
+
+    private void comprobarContras(){
+
     }
 }
