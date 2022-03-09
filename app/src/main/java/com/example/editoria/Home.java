70fc5efb2e42380b88176d7f6ebcdf50f042c5f4
Mario <larryramirezgarcia10@gmail.com>
2022-03-09 18:58:05 +0100

    creando página filtro

--- app/src/main/java/com/example/editoria/Home.java
+++ app/src/main/java/com/example/editoria/Home.java
@@ -1,21 +1,49 @@
 package com.example.editoria;
 
+import android.app.Dialog;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Home extends AppCompatActivity {
+
+    private Dialog dialog;
+    private Button cancelar, bFiltro;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home);
 
-        getSupportActionBar().hide();
+        bFiltro = findViewById(R.id.button6);
+
+
+        /*
+        bFiltro = findViewById(R.id.bfiltro);
+        dialog = new Dialog(this);
+
+        bFiltro.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                Log.i("PRUEBA", "asdasd");
+                dialog.setContentView(R.layout.paginafiltro);
+
+                dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+            }
+        });*/
+
     }
 
-    /*
-    *     https://www.youtube.com/watch?v=Y19tdGRy2LQ
-    https://www.youtube.com/watch?v=Y19tdGRy2LQ
-    https://www.youtube.com/watch?v=Y19tdGRy2LQ*/
+    public void ss(View view){
+        Log.i("ejemplo", "rara arara ");
+    }
+
+
+
 }
