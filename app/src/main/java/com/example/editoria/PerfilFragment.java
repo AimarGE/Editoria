ba1d74377a349cbea711b92ee7041908eafbf173
MarioWopi <larryramirezgarcia10@gmail.com>
2022-04-06 13:13:06 +0200

    Toggle de favoritos arreglado y empezando estructura del perfil del editor

--- app/src/main/java/com/example/editoria/PerfilFragment.java
+++ app/src/main/java/com/example/editoria/PerfilFragment.java
@@ -1,5 +1,7 @@
 package com.example.editoria;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,13 +9,13 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Toast;
-import android.widget.ToggleButton;
+
+import com.airbnb.lottie.LottieAnimationView;
 
 public class PerfilFragment extends Fragment {
 
-    ToggleButton favorito;
-
+    LottieAnimationView favorito;
+    boolean guardado;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,20 +30,49 @@ public class PerfilFragment extends Fragment {
 
         favorito = view.findViewById(R.id.favorito);
 
-
-
-
+        guardado = false;
 
 
         favorito.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText( getActivity(), "toggle favorito", Toast.LENGTH_SHORT).show();
+                guardado = likeAnimation(favorito, R.raw.heart_animation, guardado);
             }
         });
+
+        /*favorito.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText( getActivity(), "toggle favorito", Toast.LENGTH_SHORT).show();
+            }
+        });*/
 
 
         // Inflate the layout for this fragment
         return view;
+    }
+
+
+    private boolean likeAnimation(LottieAnimationView imageView, Integer animation, Boolean like){
+
+        if (!like){
+            imageView.setAnimation(animation);
+            imageView.playAnimation();
+        }else{
+            imageView.animate()
+                    .alpha(0f)
+                    .setDuration(200)
+                    .setListener(new AnimatorListenerAdapter() {
+                        @Override
+                        public void onAnimationEnd(Animator animation) {
+                            imageView.setImageResource(R.drawable.favorite_border);
+                            imageView.setAlpha(1f);
+                        }
+                    });
+
+
+        }
+
+        return !like;
     }
 }
