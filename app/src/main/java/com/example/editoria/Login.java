45199f35d420f4c1ba07ebd368f58947c9fcc24c
AimarGE <aimar5454@gmail.com>
2022-04-26 15:37:41 +0200

    Registros completados y Log in completado.

--- app/src/main/java/com/example/editoria/Login.java
+++ app/src/main/java/com/example/editoria/Login.java
@@ -5,31 +5,31 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class Login extends AppCompatActivity {
 
-    private EditText campoEmail;
+    private EditText campoNombreUsuario;
     private EditText campoPassword;
-    private FirebaseDatabase Fdatabase;
-    private DatabaseReference dRef;
+    private FirebaseDatabase firebaseDatabase;
+    private DatabaseReference dRef = FirebaseDatabase.getInstance().getReferenceFromUrl("https://editoria-bb3aa-default-rtdb.europe-west1.firebasedatabase.app/");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
-        campoEmail= (EditText) findViewById(R.id.emailLogIn);
+        campoNombreUsuario= (EditText) findViewById(R.id.UserLogIn);
         campoPassword= (EditText) findViewById(R.id.contraLogin);
-        Fdatabase= FirebaseDatabase.getInstance("https://editoria-bb3aa-default-rtdb.europe-west1.firebasedatabase.app/");
-        dRef= Fdatabase.getReference();
         getSupportActionBar().hide();
     }
 
@@ -39,18 +39,34 @@ public class Login extends AppCompatActivity {
     }
 
     public void login(View _){
-        String email=campoEmail.getText().toString();
+        String usuario= campoNombreUsuario.getText().toString();
         String password = campoPassword.getText().toString();
-        dRef.child("Usuarios").get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
-            @Override
-            public void onComplete(@NonNull Task<DataSnapshot> task) {
-                if (!task.isSuccessful()){
-                    Log.e("firebase", "Error getting data", task.getException());
-                }else{
-                    Log.d("firebase", String.valueOf(task.getResult().getValue()));
+
+        dRef.child("Usuarios").addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot snapshot) {
+                    if(snapshot.hasChild(usuario)){
+
+                        final String getPassword = snapshot.child(usuario).child("password").getValue(String.class);
+
+                        if(getPassword.equals(password)){
+                            Toast.makeText(Login.this, "Sesión iniciada con éxito", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(Login.this, MainFragmentContainer.class));
+                        }else{
+                            Toast.makeText(Login.this, "Wrong password", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                    else{
+                        Toast.makeText(Login.this, "Usuario o contraseña incorrectos", Toast.LENGTH_SHORT).show();
+                    }
                 }
-            }
-        });
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError error) {
+
+                }
+            });
+
     }
 
     public void forgotenPass(View _){
--- app/src/main/java/com/example/editoria/model/Usuario.java
+++ app/src/main/java/com/example/editoria/model/Usuario.java
@@ -12,17 +12,17 @@ public class Usuario {
     private String fechaNacimiento;
     private String clase;
 
-    public Usuario(String usuario, String password, String correoE, String telefono, String fechaNacimiento, String clase){
-        this.id= UUID.randomUUID().toString();
-        this.usuario=usuario;
-        this.password=password;
-        this.correoE=correoE;
-        this.telefono=telefono;
-        this.fechaNacimiento=fechaNacimiento;
-        this.clase=clase;
+    public Usuario(String usuario, String password, String correoE, String telefono, String fechaNacimiento, String clase) {
+        this.id = UUID.randomUUID().toString();
+        this.usuario = usuario;
+        this.password = password;
+        this.correoE = correoE;
+        this.telefono = telefono;
+        this.fechaNacimiento = fechaNacimiento;
+        this.clase = clase;
     }
 
-    public String getId(){
+    public String getId() {
         return this.id;
     }
 
@@ -44,5 +44,9 @@ public class Usuario {
 
     public String getFechaNacimiento() {
         return fechaNacimiento;
+    }
+
+    public String getClase() {
+        return clase;
     }
 }
