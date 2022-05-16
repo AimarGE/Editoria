401c984d66c9382bb12436af8fbac0f64205c6c4
Mario <larryramirezgarcia10@gmail.com>
2022-05-16 19:11:06 +0200

    Menu inscripción evento hecha - haciendo página principal del evento.

--- app/src/main/java/com/example/editoria/fragments/EventoFragment.java
+++ app/src/main/java/com/example/editoria/fragments/EventoFragment.java
@@ -3,12 +3,16 @@ package com.example.editoria.fragments;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 
+import com.airbnb.lottie.L;
+import com.example.editoria.MainFragmentContainer;
 import com.example.editoria.R;
 import com.squareup.picasso.Picasso;
 
@@ -17,6 +21,7 @@ public class EventoFragment extends Fragment {
 
     View view;
     ImageView imagenEvento;
+    Button botonInscribirse;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -29,9 +34,29 @@ public class EventoFragment extends Fragment {
                              Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.fragment_evento, container, false);
         imagenEvento = view.findViewById(R.id.imagenEvento);
+        botonInscribirse = view.findViewById(R.id.botonInscribirse);
+
 
         Picasso.get().load("https://upload.wikimedia.org/wikipedia/commons/f/ff/Anas_platyrhynchos_qtl1.jpg").into(imagenEvento);
 
+        init();
+
         return view;
     }
+
+    private void init() {
+
+        botonInscribirse.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MainFragmentContainer.bottomNavigation.show(3, true);
+                FragmentTransaction ft = getFragmentManager().beginTransaction();
+                ft.replace(R.id.mainFrame, new EventoPaginaPrincipalFragment()).addToBackStack("tag");
+                ft.commit();
+            }
+        });
+
+    }
+
+
 }
---
+++ app/src/main/java/com/example/editoria/fragments/EventoPaginaPrincipalFragment.java
@@ -0,0 +1,33 @@
+package com.example.editoria.fragments;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.editoria.R;
+
+public class EventoPaginaPrincipalFragment extends Fragment {
+    View view;
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        view = inflater.inflate(R.layout.fragment_evento_pagina_principal, container, false);
+
+        return view;
+    }
+
+}
