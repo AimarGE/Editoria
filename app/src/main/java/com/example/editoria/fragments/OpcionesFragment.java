da2d57fe15117d07b70f2eb39099828106aa9d93
Mario <larryramirezgarcia10@gmail.com>
2022-05-25 19:58:25 +0200

    Modificación de los paquetes en la creación de los proyectos / Wireframes de proyectos de Cliente hecha (falta lógica) / Wireframe valorar editor hecha (falta lógica) / Wireframe recuperar contraseña hecho / Cerrar sesión hecho

--- app/src/main/java/com/example/editoria/fragments/OpcionesFragment.java
+++ app/src/main/java/com/example/editoria/fragments/OpcionesFragment.java
@@ -1,20 +1,36 @@
 package com.example.editoria.fragments;
 
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
+import com.example.editoria.GlobalVariable;
+import com.example.editoria.MainActivity;
+import com.example.editoria.MainFragmentContainer;
 import com.example.editoria.R;
+import com.example.editoria.Register;
 
 public class OpcionesFragment extends Fragment {
 
-    public OpcionesFragment() {
-        // Required empty public constructor
-    }
+    View view;
+    TextView cerrarSesion;
+    SharedPreferences.Editor editor;
+    SharedPreferences preferences;
+    AlertDialog.Builder builder;
+    LinearLayout opcionesPerfil;
 
 
     @Override
@@ -24,7 +40,84 @@ public class OpcionesFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.fragment_opciones, container, false);
+        cerrarSesion = view.findViewById(R.id.cerrarSesion);
+        preferences = getActivity().getSharedPreferences("sesion", Context.MODE_PRIVATE);
+        editor = preferences.edit();
+        builder = new AlertDialog.Builder(view.getContext());
+        opcionesPerfil= view.findViewById(R.id.opc_perfil);
 
-        return inflater.inflate(R.layout.fragment_opciones, container, false);
+        init();
+
+        return view;
+    }
+
+    private void init() {
+
+        listeners();
+
+    }
+
+    private void listeners() {
+
+        //LISTENER PARA ABRIR LA OPCION DE PERFIL
+        opcionesPerfil.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                opcionesPerfil();
+            }
+        });
+
+        //LISTENERS PARA CERRAR SESION
+        cerrarSesion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialogoCerrarSesion();
+            }
+        });
+
+    }
+
+    private void opcionesPerfil() {
+
+        MainFragmentContainer.bottomNavigation.show(5, true);
+        FragmentTransaction ft = getFragmentManager().beginTransaction();
+        ft.replace(R.id.mainFrame, new OpcionesPerfilFragment()).addToBackStack("tag");
+        ft.commit();
+
+    }
+
+    private void cerrarSesion() {
+
+        editor.putBoolean("sesionIniciada",false);
+        editor.commit();
+        editor.putString("usuario", "usuarioNull");
+
+        GlobalVariable.nombreUsuario="usuarioNull";
+
+        Intent intent = new Intent(view.getContext(), MainActivity.class);
+        startActivity(intent);
+
+    }
+
+
+
+    private void dialogoCerrarSesion() {
+
+        builder.setTitle("¿Estás seguro que quieres cerrar sesión?")
+                .setCancelable(true)
+                .setPositiveButton("Si", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        cerrarSesion();
+                    }
+                })
+                .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                    }
+                })
+                .show();
     }
 }
