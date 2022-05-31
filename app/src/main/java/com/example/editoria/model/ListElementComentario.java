4fd38e5a83a441d0fb0e5f6c255a6c4b5e494e60
AimarGE <aimar5454@gmail.com>
2022-05-31 02:45:39 +0200

    Mostrar paquetes bien

--- app/src/main/java/com/example/editoria/model/ListElementComentario.java
+++ app/src/main/java/com/example/editoria/model/ListElementComentario.java
@@ -5,11 +5,13 @@ public class ListElementComentario {
     public String icon;
     public String name;
     public String comentario;
+    public String valoracion;
 
-    public ListElementComentario(String icon, String name, String comentario) {
+    public ListElementComentario(String icon, String name, String comentario, String valoracion) {
         this.icon = icon;
         this.name = name;
         this.comentario = comentario;
+        this.valoracion = valoracion;
     }
 
     public String getIcon() {
@@ -36,4 +38,15 @@ public class ListElementComentario {
         this.comentario = comentario;
     }
 
+    public void setIcon(String icon) {
+        this.icon = icon;
+    }
+
+    public String getValoracion() {
+        return valoracion;
+    }
+
+    public void setValoracion(String valoracion) {
+        this.valoracion = valoracion;
+    }
 }
